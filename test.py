ddf03e557ce568243ba08283dc80b49ee3706ba6
oddballbutler <oddballbutler@gmail.com>
2014-06-09 15:02:56 -0400

    Adding a small test program

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from challongeMatchGetter import ChallongeMatchGetter
+
+username = "oddballbutler"
+api_key = "KtLCcYrK3s8HdFTrF4IwYN8O2j8nHa2jl5WvKI4S"
+
+match_getter = ChallongeMatchGetter(username, api_key)
+
+match_getter.setTournament("ccr-test")
+
+matches = match_getter.getMatches()
+print(matches)
