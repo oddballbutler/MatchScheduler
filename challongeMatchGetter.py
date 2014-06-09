8095f3eedd449d7f17f3b3ac47d04d7f88217614
oddballbutler <oddballbutler@gmail.com>
2014-06-09 15:02:36 -0400

    convert Challonge Matches to MatchScheduler Matches

--- challongeMatchGetter.py
+++ challongeMatchGetter.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 import challonge
+from match import Match
 
-class Challonge(object):
+class ChallongeMatchGetter(object):
     def __init__(self, username, api_key):
         challonge.set_credentials(username, api_key)
 
-    def setTournament(tournament):
+    def setTournament(self, tournament):
         self.tournament = challonge.tournaments.show(tournament)
 
-    def getMatches():
-        self.matches = challonge.matches.index(self.tournament["id"])
-        return
+    def getMatches(self):
+        self.matches = []
+        challongeMatches = challonge.matches.index(self.tournament["id"], state="open")
+        for match in challongeMatches:
+            player1 = challonge.participants.show(self.tournament["id"], match["player1-id"])
+            player2 = challonge.participants.show(self.tournament["id"], match["player2-id"])
+            participants = [player1["name"],player2["name"]]
+            self.matches.append(Match(match["id"],participants))
+
+        return self.matches
