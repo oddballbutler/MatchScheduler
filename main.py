c037b50e49e0e5ed2d5adf5d5be8823d6b649cd3
oddballbutler <oddballbutler@gmail.com>
2014-07-02 16:06:13 -0400

    Show scheduled matches in a textbox

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-"""
-ZetCode PyQt4 tutorial
-
-In this example, we create a simple
-window in PyQt4.
-
-author: Jan Bodnar
-website: zetcode.com
-last edited: October 2011
-"""
-
 import sys
+from matchscheduler import MatchScheduler
+from challongeMatchGetter import ChallongeMatchGetter
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QStringListModel
 
 def main():
+    match_getter = ChallongeMatchGetter("oddballbutler", "KtLCcYrK3s8HdFTrF4IwYN8O2j8nHa2jl5WvKI4S")
+    match_getter.setTournament("ccr-test")
+    scheduler = MatchScheduler(match_getter)
     app = QApplication(sys.argv)
+
+    scheduler.updateMatches()
+    matches = scheduler.getScheduledMatches()
+
+    myStringList = []
+
+    for x in matches:
+        myStringList.append(x.__str__())
+
+    myListModel = QStringListModel(myStringList)
+
     window = uic.loadUi("mainwindow.ui")
+    window.listView.setModel(myListModel)
     window.show()
 
     sys.exit(app.exec_())
